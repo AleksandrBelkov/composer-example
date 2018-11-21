02e8fe027f820734fcb95f826c13fe90f95101a0
Alex Belkoff <belkoff@mail.ru>
2018-11-21 07:04:20 +0300

    Parser class

---
+++ Parser.php
@@ -0,0 +1,29 @@
+<?php
+
+require_once 'ParserInterface.php';
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Parser
+ *
+ * @author admin
+ */
+class Parser implements ParserIterface {
+    public function process(string $url, string @tag) {
+    
+        return [
+           'just',
+            'do',
+            'it'
+        ]
+           
+        
+    }    
+
+
+//put your code here
+}
